ebfd94f0d770b2074d3094774cec935ef97a35da
Shahin Mahmud <mahmud.shahin@genweb2.com>
2021-11-24 17:10:30 +0600

    publice, view added

---
+++ public/main.js
@@ -0,0 +1,14 @@
+// main.js
+const update = document.querySelector('#update-button')
+
+update.addEventListener('click', _ => {
+  // Send PUT Request here
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        name: 'Darth Vadar',
+        quote: 'I find your lack of faith disturbing.'
+      })
+  });
+})
--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const MongoClient = require('mongodb').MongoClient;
 
 // App bootstrap
 const app = express();
-
+// Config
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 // Routing;
 // We normally abbreviate `request` to `req` and `response` to `res`.
 
-app.get('/', (req, res) => {
-    // console.log(`listening from root.`);
-    // res.send(`Here is the first response.`);
-    res.sendFile(__dirname + '/index.html')
-});
+// app.get('/', (req, res) => {
+//     console.log(`listening from root.`);
+//     res.send(`Here is the first response.`);
+//     res.sendFile(__dirname + '/index.html')
+// });
 
 
 // Database connections
@@ -28,22 +31,37 @@ Client.connect()
             
         console.log(`connected to mongodb atlas...`);
 
-        const collection = client.db("star-wars-quotes").collection("quotes");
+        const db = client.db("star-wars-quotes");
+        const collection = db.collection("quotes");
+
         // perform actions on the collection object
         // client.close();
+
+        app.get('/', (req, res) => { 
+            collection.find().toArray()
+                .then(result => {
+                    // console.log(result);
+                    res.render('index.ejs', { quotes: result });
+                })
+                .catch(err => {
+                    console.error(err);
+                });            
+        });
+
         app.post('/quotes', (req, res) => {
             // console.log(`Hellooooooooooooo!`);
             // console.log(req.body);
 
             collection.insertOne(req.body)
                 .then(result => { 
-                    console.log(result);
+                    // console.log(result);
                     res.redirect('/');
                 })
                 .catch(err => { 
                     console.error(err);
                 });
         });
+       
     })
     .catch((err) => {
         console.error(err);
